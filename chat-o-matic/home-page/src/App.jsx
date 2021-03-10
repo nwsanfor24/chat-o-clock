444399918b7aad2f54780acb1eec4147ebbcf8ab
Nathan Sanford <nwsanfor24@gmail.com>
2021-03-10 10:31:16 -0500

    installed webpack

--- chat-o-matic/home-page/src/App.jsx
+++ chat-o-matic/home-page/src/App.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Container } from "shards-react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => {
+  <Container>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt eveniet
+      consectetur vel fuga, dolore placeat? Ex impedit odio corporis qui!
+    </p>
+    <h1>Chat!</h1>
+    <div>Chat window here</div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatibus
+      vel possimus molestiae voluptate sit saepe minus. Cumque, molestias
+      ratione.
+    </p>
+  </Container>;
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
